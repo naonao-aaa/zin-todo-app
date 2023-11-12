a3648041f12964da7a745419cc9b34504fa4bea6
naonao-aaa <naonao1814naonao@gmail.com>
2023-11-12 13:01:40 +0900

    用いるカスタムフック等を作成。 ZustandとReact-queryを使用

--- types/types.ts
+++ types/types.ts
@@ -13,3 +13,9 @@ export type Notice = {
   content: string
   user_id: string | undefined
 }
+
+//EditedTaskのデータ型は、Task型から'created_at'と'user_id'を取り除いたデータ型とする。
+export type EditedTask = Omit<Task, 'created_at' | 'user_id'>
+
+//EditedNoticeのデータ型は、Notice型から'created_at'と'user_id'を取り除いたデータ型とする。
+export type EditedNotice = Omit<Notice, 'created_at' | 'user_id'>
